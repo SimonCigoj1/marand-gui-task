062bd5fcc1af8a39422e74be50aa249b851182fb
Simon Cigoj <simon.cigoj@arctur.si>
2014-07-04 14:19:52 +0200

    saturation and heart rate graph

--- src/main/webapp/resources/js/functions.js
+++ src/main/webapp/resources/js/functions.js
@@ -6,7 +6,8 @@ var diastoloc = [];
 var systolic = [];
 var dates = [];
 var temperature = [];
-var temperatureHigh = [];
+var heartRate = [];
+var saturation = [];
 var startDate;
 
 
@@ -43,6 +44,9 @@ function drawBloodPressureData(data){
 		                day: '%e. %b'
 		            }
 		        },
+		        tooltip: {
+	                valueSuffix: 'mm[Hg]'
+	            },
 		        series: [
 		        {
 		            name: 'Systolic',
@@ -223,19 +227,175 @@ function drawTemperatureData(data){
           fillOpacity: 0.3
       });
       
-      /*plotBands: [{ // Light air
-          from: 0.3,
-          to: 1.5,
-          color: 'rgba(68, 170, 213, 0.1)',
-          label: {
-              text: 'Light air',
-              style: {
-                  color: '#606060'
-              }
-          }
-      }*/
-      
-      
-
     
+}
+
+function drawHeartRateData(data){
+		  
+	var startDate = parseDate(data.measurements[0].date); 
+	  
+    $.each( data.measurements, function( i, item ) {
+  	   	heartRate.push(item.heartRate);
+    });
+
+   // alert(temperatureHigh);
+    
+	 var options = {
+		        chart: {
+		            renderTo: 'heartRateChart',
+		            type: 'area'
+		        },
+		        plotOptions: {
+		            series: {
+		                fillOpacity: 0.7
+		            }
+		        },
+		        title: {
+		            text: 'Heart Rate'
+		        },
+		        subtitle: {
+		            text: 'Patient heart rate measurements'
+		        },
+		        yAxis: {
+		        	title: {
+		                text: 'Heart Rate /min'
+		            }
+		        },
+		        xAxis: {
+		            type: 'datetime',
+		            dateTimeLabelFormats: {
+		                day: '%e. %b'
+		            }
+		        },
+		        tooltip: {
+	                valueSuffix: '/min'
+	            },
+		        series: [
+		        {
+		            name: 'Heart Rate',
+		            data: [{}],
+		            type: 'area',
+		            color: '#FFBF6C',
+		            fillOpacity: 0.7,
+		            zIndex:2,
+		            marker: {
+	                    enabled: false
+	                },
+	                animation: false,
+	                pointInterval: 24 * 3600 * 1000 // one day
+		        }
+		       ]
+		        
+		    }; 	
+    
+    options.series[0].pointStart = startDate;
+    options.series[0].data = heartRate;
+    
+    var maxY = Math.max.apply(Math,heartRate)+10; 
+    var minY= Math.min.apply(Math,heartRate)-10;
+    
+    options.yAxis.min = minY;
+    options.yAxis.max = maxY;
+    
+    var temperatureChart = new Highcharts.Chart(options);
+
+    temperatureChart.yAxis[0].addPlotLine({
+          value: 80,
+          color: 'blue',
+          width: 1,
+          id: 'plot-line-1',
+          dashStyle: 'ShortDash',
+          zIndex:10,
+          label: {
+          	text: 'feaver 37'
+	            }
+      });
+ 
+    
+  
+}
+
+function drawSaturationData(data){
+	  
+	var startDate = parseDate(data.measurements[0].date); 
+	  
+    $.each( data.measurements, function( i, item ) {
+    	saturation.push(item.spq2numerator);
+    });
+
+   // alert(temperatureHigh);
+    
+	 var options = {
+		        chart: {
+		            renderTo: 'saturationChart',
+		            type: 'area'
+		        },
+		        plotOptions: {
+		            series: {
+		                fillOpacity: 0.7
+		            }
+		        },
+		        title: {
+		            text: 'Saturation'
+		        },
+		        subtitle: {
+		            text: 'Oxigen saturation'
+		        },
+		        yAxis: {
+		        	title: {
+		                text: 'Saturatoin in %'
+		            }
+		        },
+		        xAxis: {
+		            type: 'datetime',
+		            dateTimeLabelFormats: {
+		                day: '%e. %b'
+		            }
+		        },
+		        tooltip: {
+	                valueSuffix: '%'
+	            },
+		        series: [
+		        {
+		            name: 'saturation',
+		            data: [{}],
+		            type: 'area',
+		            color: '#FFBF6C',
+		            fillOpacity: 0.7,
+		            zIndex:2,
+		            marker: {
+	                    enabled: false
+	                },
+	                animation: false,
+	                pointInterval: 24 * 3600 * 1000 // one day
+		        }
+		       ]
+		        
+		    }; 	
+    
+    options.series[0].pointStart = startDate;
+    options.series[0].data = saturation;
+    
+    var maxY = Math.max.apply(Math,saturation)+10; 
+    var minY= Math.min.apply(Math,saturation)-10;
+    
+    options.yAxis.min = minY;
+    options.yAxis.max = maxY;
+    
+    var saturationChart = new Highcharts.Chart(options);
+
+    saturationChart.yAxis[0].addPlotLine({
+          value: 80,
+          color: 'blue',
+          width: 1,
+          id: 'plot-line-1',
+          dashStyle: 'ShortDash',
+          zIndex:10,
+          label: {
+          	text: 'feaver 37'
+	            }
+      });
+ 
+    
+  
 }
